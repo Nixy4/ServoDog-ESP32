bc7343b9d64da6b08d81b7228070eb4fc9dc06ed
nix <609610903@qq.com>
2025-04-25 11:50:27 +0800

    modified:   cp2win.sh

--- cp2win.sh
+++ cp2win.sh
@@ -1,48 +1,30 @@
-cmake_file=./CMakeLists.txt
-
+cmakefile=./CMakeLists.txt
 windir=/mnt/d/ESP/flash_download_tool/temp
-#D:\ESP\flash_download_tool\temp
-
-target=$(grep -E "^project\(" "$cmake_file" | sed -E 's/project\(([^)]+)\)/\1/')
-target=$(echo $target | tr -d '\r')
 builddir=./build
-bootloader=${builddir}/bootloader/bootloader.bin # 0x0000
-partition=${builddir}/partition_table/partition-table.bin # 0x8000
-ota_data=${builddir}/ota_data_initial.bin # 0xD000
-model=${builddir}/srmodels/srmodels.bin # 0x10000
-main=${builddir}/${target}.bin # 0x410000
-storage=${builddir}/storage.bin # 0xC10000
 
+#! 清空windows端目标文件夹
 rm -f ${windir}/**.bin
 echo 目标文件夹: ${windir} 已清空
 
-if [ -f $bootloader ]; then
-  echo 已复制 bootloader
-  cp $bootloader ${windir}/bootloader.bin
-fi
+#! 提取项目名称
+target=$(grep -E "^project\(" "$cmakefile" | sed -E 's/project\(([^)]+)\)/\1/')
+target=$(echo $target | tr -d '\r')
+echo 项目名称: $target
 
-if [ -f $partition ]; then
-  echo 已复制 partition-table
-  cp $partition ${windir}/partition-table.bin
-fi
-
-if [ -f $ota_data ]; then
-  echo 已复制 ota_data
-  cp $ota_data ${windir}/ota_data_initial.bin
-fi
-
-if [ -f $model ]; then
-  echo 已复制 model
-  cp $model ${windir}/srmodels.bin
-fi
-
-if [ -f $main ]; then
-  echo 已复制 main
-  cp $main ${windir}/main.bin
-fi
-
-if [ -f $storage ]; then
-  echo 已复制 storage
-  cp $storage ${windir}/storage.bin
-fi
-
+#! 从第二行开始, 读取第二列参数
+flash_args=$(awk 'NR>1 {print $2}' $builddir/flash_args)
+for filepath in $flash_args
+do
+  filename=$(basename "$filepath")
+  if [ "$filename" = "$target.bin" ];then
+    wfilename="main.bin"
+  else
+    wfilename=$filename
+  fi
+  filepath=${builddir}/${filepath}
+  wfilepath=${windir}/${wfilename}
+  if [ -f $filepath ]; then
+    cp $filepath ${wfilepath}
+    echo 已复制 $filename '>>' $wfilename
+  fi
+done
